c73f201597dc61b1afddae38221c9e492fe834df
Bon-eng <keitaymzk@gmai.com>
2021-01-11 23:13:38 +0900

    about delete commit-fix

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,5 +1,7 @@
 class PartiesController < ApplicationController
 
+  before_action :party_find, only: [:show, :edit, :update, :destroy]
+
   def index
     @party = Party.order("created_at DESC")
   end
@@ -18,18 +20,12 @@ class PartiesController < ApplicationController
   end
 
   def show
-    @party = Party.find(params[:id])
   end
 
   def edit
-    @party = Party.find(params[:id])
-    # if @party.save
-    #   redirect_to root_path
-    # end
   end
 
   def update
-    @party = Party.find(params[:id])
     if @party.update(party_params)
       redirect_to root_path
     else
@@ -38,17 +34,21 @@ class PartiesController < ApplicationController
   end
 
   def destroy
-    @party = Party.find(params[:id])
     @party.destroy
     redirect_to root_path
   end
 
+  
   private
 
   def party_params
     params.require(:party).permit(
       :name, :introduction, :season_id, :country_id, :genre_id, :image
     ).merge(user_id: current_user.id)
+  end
+
+  def party_find
+    @party = Party.find(params[:id])
   end
 
 end
