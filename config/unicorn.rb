4e462a024d155b8b3deff130c1aa12a7645cb258
Bon-eng <keitaymzk@gmai.com>
2021-02-18 18:02:23 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
@@ -22,7 +22,6 @@ stdout_path "#{app_path}/log/unicorn.stdout.log"
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
 
-#以下は応用的な設定なので説明は割愛
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
