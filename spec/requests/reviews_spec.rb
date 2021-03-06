dcc04f2dad64e64df17eb259fec7fed83434affd
Bon-eng <keitaymzk@gmail.com>
2021-03-06 18:29:57 +0900

    reviewsコントローラーのテスト追加

--- spec/requests/reviews_spec.rb
+++ spec/requests/reviews_spec.rb
@@ -1,87 +1,58 @@
 require 'rails_helper'
-
 RSpec.describe ReviewsController, type: :request do
-  # let(:admin_user) { FactoryBot.create(:user, admin: true) }
-  # let(:party) { FactoryBot.create(:party, user_id: user.id) }
-  # let(:user) { FactoryBot.create(:user) }
-  # let(:review) { FactoryBot.create(:review, party_id: party.id) }
   before do
     @user = FactoryBot.create(:user)
-    @admin_user = FactoryBot.create(:user, admin: true)
-    @party = FactoryBot.create(:party)
-    @review = FactoryBot.create(:review)
+    @party = FactoryBot.create(:party, user_id: @user.id)
+    @review = FactoryBot.create(:review, party_id: @party.id, user_id: @user.id)
   end
+#全7項目
 
   describe 'POST #create' do
-    before do
-      sign_in @user
-    end
     it 'createアクションにリクエストすると正常にレスポンスが返ってくる' do
-      post party_reviews_path(@party, @review.party), params: { id: @party_id, review: FactoryBot.attributes_for(:review) }
+      sign_in @user
+      post party_reviews_path(@party.id, @review.id), params: { id: @party_id, review: FactoryBot.attributes_for(:review) }
       expect(response.status).to eq 200
     end
   end
 
   describe 'GET #edit' do
-    it 'editアクションにリクエストすると正常にレスポンスが返ってくる' do
-      get edit_party_review_path(@party, @review.party)
+    context 'ログインしているとき' do
+      before do
+        sign_in @user
+      end
+      it 'editアクションにリクエストすると正常にレスポンスが返ってくる' do
+        get edit_party_review_path(@party.id, @review.id)
+        expect(response.status).to eq 200
+      end
+      it "editアクションにリクエストするとレスポンスに投稿済みレビュータイトルが存在する" do 
+        get edit_party_review_path(@party.id, @review.id)
+        expect(response.body).to include @review.title
+      end
+      it "editアクションにリクエストするとレスポンスに投稿済みレビュー本文が存在する" do 
+        get edit_party_review_path(@party.id, @review.id)
+        expect(response.body).to include @review.content
+      end
+    end
+    context 'ログインしていないとき' do
+      it 'editアクションにリクエストすると正常にレスポンスが返ってくる' do
+        get edit_party_review_path(@party.id, @review.id)
+        expect(response.status).to eq 302
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    it 'updateアクションにリクエストすると正常にレスポンスが返ってくる' do
+      sign_in @user
+      put party_review_path(@party.id, @review.id), params: { id: @party_id, review: FactoryBot.attributes_for(:review) }
       expect(response.status).to eq 302
     end
   end
 
   describe 'DELETE #destroy' do
     it 'destroyアクションにリクエストすると正常にレスポンスが返ってくる' do
-      delete party_review_path(@party, @review.party)
+      delete party_review_path(@party.id, @review.id)
       expect(response.status).to eq 302
     end
   end
-
-  # describe 'GET #show' do
-  #   before do
-  #     sign_in user
-  #   end
-  #   it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do
-  #     get party_review_path(@party, @review.party)
-  #     expect(response.status).to eq 200
-  #   end
-  #   it "showアクションにリクエストするとレスポンスに投稿済みpartyの画像が存在する" do 
-  #     get party_review_path(@party, @review.party)
-  #     expect(response.body).to include 'test_image.jpg'
-  #   end
-  #   it "showアクションにリクエストするとレスポンスに投稿済みpartyの名前が存在する" do 
-  #     get party_review_path(@party, @review.party)
-  #     expect(response.body).to include @party.name
-  #   end
-  #   it "showアクションにリクエストするとレスポンスに投稿済みpartyの紹介文が存在する" do 
-  #     get party_review_path(@party, @review.party)
-  #     expect(response.body).to include @party.introduction
-  #   end
-  #   it "showアクションにリクエストするとレスポンスに投稿済みpartyの開催時期が存在する" do 
-  #     get party_review_path(@party, @review.party)
-  #     expect(response.body).to include @party.season.name
-  #   end
-  #   it "showアクションにリクエストするとレスポンスに投稿済みpartyの開催地域が存在する" do 
-  #     get party_review_path(@party, @review.party)
-  #     expect(response.body).to include @party.country.name
-  #   end
-  #   it "showアクションにリクエストするとレスポンスに投稿済みpartyのジャンルが存在する" do 
-  #     get party_review_path(@party, @review.party)
-  #     expect(response.body).to include @party.genre.name
-  #   end
-  #   it "showアクションにリクエストするとレスポンスに投稿済みpartyのオフィシャルHPが存在する" do 
-  #     get party_review_path(@party, @review.party)
-  #     expect(response.body).to include @party.official_url
-  #   end
-  # end
-
-  # describe 'PUT #update' do
-  #   before do
-  #     sign_in user
-  #   end
-  #   it 'updateアクションにリクエストすると正常にレスポンスが返ってくる' do
-  #     put party_review_path(@party, @review.party), params: { id: @party_id, review: FactoryBot.attributes_for(:review) }
-  #     expect(response.status).to eq 200
-  #   end
-  # end
-
 end
