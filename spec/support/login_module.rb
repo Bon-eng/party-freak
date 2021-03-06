902634d581b720656c98e187ce814a29834b5395
Bon-eng <keitaymzk@gmail.com>
2021-03-06 21:02:52 +0900

    usersコントローラーのテスト追加

--- spec/support/login_module.rb
+++ spec/support/login_module.rb
@@ -5,11 +5,4 @@ module LoginModule
     fill_in 'パスワード', with: user.password
     find('input[name="commit"]').click
   end
-
-  # def login(admin_user)
-  #   visit new_user_session_path
-  #   fill_in 'メールアドレス', with: admin_user.email
-  #   fill_in 'パスワード', with: admin_user.password
-  #   find('input[name="commit"]').click
-  # end
 end
