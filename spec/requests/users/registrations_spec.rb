96ed2d526aedb5bd95a4cda6f312c0b6615cd22a
Bon-eng <keitaymzk@gmail.com>
2021-03-06 18:53:13 +0900

    registrationsコントローラーのテスト修正

--- spec/requests/users/registrations_spec.rb
+++ spec/requests/users/registrations_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
-
 RSpec.describe "Users::Registrations", type: :request do
-  let(:user) { FactoryBot.create(:user) }
   before do
     @user = FactoryBot.create(:user)
   end
+#全6項目
 
-  describe 'POST /users/registrations/create' do
+  describe 'GET /users/registrations#new' do
+    context 'パラメータが正常な場合' do
+      it '新規登録ページにリクエストすると正常にレスポンスが返ってくる' do
+        get new_user_registration_path
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'POST /users/registrations#create' do
     context 'パラメータが正常な場合' do
       it '新規登録ページにリクエストすると正常にレスポンスが返ってくる' do
         post user_registration_path
@@ -23,10 +31,6 @@ RSpec.describe "Users::Registrations", type: :request do
       end
     end
     context 'パラメータが異常な場合' do
-      it '新規登録ページにリクエストすると正常にレスポンスが返ってくる' do
-        post user_registration_path
-        expect(response.status).to eq 200
-      end
       it 'ユーザーが新規登録できない' do
         expect do
           post user_registration_path
@@ -35,7 +39,7 @@ RSpec.describe "Users::Registrations", type: :request do
     end
   end
 
-  describe 'PUT /users/registrations/update' do
+  describe 'PUT /users/registrations#update' do
     it 'updateアクションにリクエストすると正常にレスポンスが返ってくる' do
       put user_registration_path, params: { id: @user_id, user: FactoryBot.attributes_for(:user) }
       expect(response.status).to eq 302
